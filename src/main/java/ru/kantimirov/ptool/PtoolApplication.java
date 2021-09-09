b32d67b15b9001db2b0183a2490a4524d3d6f85d
DanKantimirov <kantimirov_da@yahoo.com>
2021-09-09 09:41:52 +0300

    init commit

---
+++ src/main/java/ru/kantimirov/ptool/PtoolApplication.java
@@ -0,0 +1,13 @@
+package ru.kantimirov.ptool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kantimirov/ptool/PtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kantimirov.ptool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
